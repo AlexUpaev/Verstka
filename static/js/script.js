886dbfcb89824df16916d46f56a93e9a5131bd72
AlexUpaev <167004192+AlexUpaev@users.noreply.github.com>
2024-10-23 12:57:55 +0300

    2 comitt

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+const modal  = document.querySelector('.modal');
+const buttonCart = document.querySelector('.button__cart');
+const buttonClosed = document.querySelector('.button--closed');
+const cancelButton = document.querySelector('.cancelButton');
+
+buttonCart.addEventListener('click', toggleModal);
+buttonClosed.addEventListener('click', toggleModal);
+cancelButton.addEventListener('click', toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle('open');
+}
